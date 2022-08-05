92c4fb1bba4c2c773c6572914c3d1600ffd8d2ec
ShubhamP48 <87799260+ShubhamP48@users.noreply.github.com>
2022-08-05 19:44:35 +0530

    Add files via upload

---
+++ bulb-onff/script.js
@@ -0,0 +1,11 @@
+
+const button= document.querySelector("#btn");
+const bulb= document.querySelector("#bulb");
+
+button.addEventListener('click',function(events){
+    // console.log(bulb.src);
+    bulb.src="./bulb-on.jpg";
+    
+})
+
+
